8ff8b1828c8f5ea828b6a2d8b69b1e5728ae56b0
pradeepyalamasetti <pradeepyalamasetti111@gmail.com>
2022-11-24 14:16:07 +0530

    first commit

--- index.js
+++ index.js
@@ -15,6 +15,39 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const users = []
+
+app.get('/users', (req, res) => {
+  res.json(users)
+})
+
+app.post('/users', async (req, res) => {
+    try {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10)
+      const user = { name: req.body.name, password: hashedPassword }
+      users.push(user)
+      res.status(201).send()
+    } catch {
+      res.status(500).send()
+    }
+  })
+
+  app.post('/users/login', async (req, res) => {
+    const user = users.find(user => user.name === req.body.name)
+    if (user == null) {
+      return res.status(400).send('Cannot find user')
+    }
+    try {
+      if(await bcrypt.compare(req.body.password, user.password)) {
+        res.send('Success')
+      } else {
+        res.send('Not Allowed')
+      }
+    } catch {
+      res.status(500).send()
+    }
+  })
+
 app.get("/api/get", (req, res) => {
     const sqlGet = "SELECT * FROM contact_db";
     db.query(sqlGet, (error, result) => {
@@ -82,4 +115,4 @@ app.get("/", (req, res) => {
 //   console.log("Server is running on port 5000");
 // });
 
-app.listen(5000);
+app.listen(process.env.PORT || 5000);
